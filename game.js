6bada2a74578e82db96084c732342c8a03469b8c
MallikAdi <aditya.mallik.10@gmail.com>
2024-03-06 10:49:23 +0530

    commit

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
+const gamePattern = [];
 
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
+  var randomNumber = Math.floor(4 * Math.random());
+  console.log(randomNumber);
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
